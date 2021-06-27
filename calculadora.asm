45c2af71c8f0fb28332ec3832095bc24f824ee5a
Daniela <dlandeta@espol.edu.ec>
2021-06-27 15:27:25 -0500

    terminadooooo

--- calculadora.asm
+++ calculadora.asm
@@ -1,21 +1,28 @@
 .data
 .eqv len 10
+error: .asciiz "Se ha producido un error"
 edad: .asciiz "Ingrese su edad: "
 sexo: .asciiz "Si es mujer escriba 0 y si es hombre escriba 1: "
 estatura: .asciiz "Ingrese su estatura en m: "
 peso: .asciiz "Ingrese su peso en kg: "
-pi: .asciiz "Su peso ideal (PI) es: "
-ppi: .asciiz "Su porcentaje de peso ideal (PPI) es: "
-imc: .asciiz "Su índice de masa corporal (IMC) es: "
-asc: .asciiz "Su área de superficie corporal (ASM) es: "
+pi: .asciiz "Su peso ideal(PI) es: "
+ppi: .asciiz "Su porcentaje de peso ideal(PPI) es: "
+imc: .asciiz "Su índice de masa corporal(IMC) es: "
+asc: .asciiz "Su área de superficie corporal(ASM) es: "
 mcm: .asciiz "Su masa corporal magra(MCM) es: "
 act: .asciiz "Su agua corporal total(ACT) es: "
-tmb: .asciiz "Su (TMB) es: "
-cal: .asciiz "Necesita "
+tmb: .asciiz "Su tasa metabólica basal(TMB) es: "
+dentro: .asciiz "Estás en el rango del peso ideal, su consumo óptimo de calorías al día debe ser de: "
+fuera_up: .asciiz "Estás por encima del rango del peso ideal, su consumo óptimo de calorías al día debe ser de: "
+fuera_dowm: .asciiz "Estás por debajo del rango del peso ideal, su consumo óptimo de calorías al día debe ser de: "
+mantener: .float 1.2
+adelgazar: .float 1.02
+subir: .float 1.375
 diez: .float 10
 cien: .float 100
 c_tmb: .float 6.25
 c_tmb_2: .float 5
+c_tmb_3: .float 161
 c_mcm_v: .float 1.07
 c_mcm_m: .float 1.1
 c_mcm_mm: .float 128
@@ -28,6 +35,7 @@ c_act_4: .float 0.3362
 u_imc: .asciiz " kg/m**2"
 lt: .asciiz " litros"
 kg: .asciiz " kg"
+cal: .asciiz " calorías"
 porcentaje: .asciiz " %"
 newLine: .asciiz "\n"
 texto: .space len
@@ -83,6 +91,7 @@ main:
 	la $a0, texto 
 	li $a1, len
 	syscall 
+	mov.s $f19, $f12
 	li $v0, 4
 	la $a0, kg
 	syscall
@@ -158,6 +167,37 @@ main:
 	syscall 
 	li $v0, 4
 	la $a0, lt
+	syscall
+	li $v0, 4
+	la $a0, newLine
+	syscall
+	
+	#calculo tmb
+	li $v0, 4
+	la $a0, tmb
+	syscall 
+	jal tmb_t
+	li $v0, 2
+	mov.s $f12, $f0
+	la $a0, texto 
+	li $a1, len
+	syscall 
+
+	li $v0, 4
+	la $a0, newLine
+	syscall
+	
+	#calorias
+	 
+	jal calorias
+	mov.s $f12, $f0
+	li $v0, 2
+	mov.s $f12, $f0
+	la $a0, texto 
+	li $a1, len
+	syscall 
+	li $v0, 4
+	la $a0, cal
 	syscall
 	li $v0, 4
 	la $a0, newLine
@@ -306,41 +346,101 @@ act_t:
 	#--------
 	jr $ra
 	
-tmb_m:
+tmb_t:
+	addi $sp,$sp,-4
+   	sw $ra,($sp)
+ 
+	beq $s1, $zero, tmb_mujer
+	
+	l.s $f4, diez
+	l.s $f5, c_tmb
+	l.s $f6, c_tmb_2
+	l.s $f7, cien
+	l.s $f9, c_tmb_3
+	mul.s $f4, $f4, $f2 #10*peso
+	mul.s $f5, $f5, $f1 #6.25*estatura
+	mul.s $f5, $f5, $f7
+	#mul.s $f7, $f5, $f7 #6.25*cm
+	mul.s $f8, $f18, $f6 #5*edad
+	add.s $f5, $f5, $f4 #6.25*estatura + 10*peso
+	#add.s $f8, $f6, $f8 #5+5*edad
+	sub.s $f5, $f5, $f8 
+	sub.s $f5, $f5, $f9
+	mov.s $f0, $f5
+	lw $ra, ($sp)
+	addi $sp,$sp,4
+	#--------
+	jr $ra
+	
+tmb_mujer:
 	l.s $f4, diez
 	l.s $f5, c_tmb
 	l.s $f6, c_tmb_2
 	l.s $f7, cien
 	mul.s $f4, $f4, $f2 #10*peso
 	mul.s $f5, $f5, $f1 #6.25*estatura
-	mul.s $f7, $f5, $f7 #6.25*cm
+	mul.s $f5, $f5, $f7 #6.25*cm
 	mul.s $f8, $f18, $f6 #5*edad
 	add.s $f5, $f5, $f4 #6.25*estatura + 10*peso
 	#add.s $f8, $f6, $f8 #5+5*edad
 	sub.s $f5, $f5, $f8 
 	add.s $f5, $f5, $f6
-	li $v0, 4
-	la $a0, tmb
-	syscall 
-	li $v0, 2
-	mov.s $f12, $f5
-	la $a0, texto 
-	li $a1, len
-	syscall 
-
-	li $v0, 4
-	la $a0, newLine
-	syscall
+	mov.s $f0, $f5
+	jr $ra
 	
 exit: 
 	li $v0, 10
 	syscall
 
+calorias:
+	addi $sp,$sp,-4
+   	sw $ra,($sp)
+   	l.s $f3, c_tmb_2
+   	l.s $f6, mantener
+   	
+ 	add.s $f4, $f19, $f3 #peso ideal +5
+ 	sub.s $f5, $f19, $f3 #peso ideal -5
+ 	c.lt.s $f4, $f2 #peso< peso ideal+5
+	bc1t else_if
+	c.lt.s $f2, $f5 #peso >peso ideal-5
+	bc1t else_if
+	mul.s $f9, $f12, $f6
+	li $v0, 4
+	la $a0, dentro
+	syscall
+	mov.s $f0, $f9
+	jr $ra
 
-
-
+else_if: 
+	l.s $f3, c_tmb_2
+	l.s $f7, adelgazar
+   	l.s $f8, subir
+	add.s $f4, $f19, $f3 #peso ideal +5
+ 	c.lt.s $f2, $f4 #peso ideal+5 < peso
+ 	bc1t subir_if
+ 	mul.s $f9, $f12, $f7
+ 	li $v0, 4
+	la $a0, fuera_up
+	syscall
+	mov.s $f0, $f9
+	jr $ra
+subir_if: 
+	l.s $f3, c_tmb_2
+   	l.s $f8, subir
+ 	sub.s $f5, $f19, $f3 #peso ideal -5
+ 	c.lt.s $f5, $f2 #peso < peso ideal -5
+ 	bc1t errorf
+ 	mul.s $f9, $f12, $f8
+ 	li $v0, 4
+	la $a0, fuera_dowm
+	syscall
+	mov.s $f0, $f9
+	jr $ra	
 	
-	
+errorf:
+	li $v0, 4
+	la $a0, error
+	syscall	
 	
 	
 	
